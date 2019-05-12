3572833e26f8a2fc3c501a87b1b70e81f003003f
montecamo <merrychristek@gmail.com>
2019-05-12 14:54:08 +0300

    add logic for grid rows change

--- src/components/PhotoGrid/GridController/index.spec.jsx
+++ src/components/PhotoGrid/GridController/index.spec.jsx
@@ -1,9 +1,42 @@
-import { render } from 'react-testing-library';
+import { render, fireEvent } from 'react-testing-library';
+import { toHaveClass } from 'jest-dom';
 
 import React from 'react';
 
 import GridController from './index.jsx';
 
+expect.extend(toHaveClass);
+
 test('renders successfully', () => {
-  render(<GridController />);
+  render(<GridController mode="multi-row" onChange={() => {}} />);
+});
+
+test('set correct rows mode', () => {
+  const { getByTestId } = render(
+    <GridController onChange={() => {}} mode="multi-row" />,
+  );
+
+  expect(getByTestId('multi-row-grid')).toHaveClass('selected');
+});
+
+test('set correct vertical mode', () => {
+  const { getByTestId } = render(
+    <GridController onChange={() => {}} mode="one-row" />,
+  );
+
+  expect(getByTestId('one-row-grid')).toHaveClass('selected');
+});
+
+test('calls change function with correct mode on click', () => {
+  const onChangeMock = jest.fn(() => null);
+  const { getByTestId } = render(
+    <GridController onChange={onChangeMock} mode="multi-row" />,
+  );
+
+  const verticalModeButton = getByTestId('one-row-grid');
+  fireEvent.click(verticalModeButton);
+
+  expect(onChangeMock).toHaveBeenCalledTimes(1);
+
+  expect(onChangeMock).toHaveBeenCalledWith('one-row');
 });
