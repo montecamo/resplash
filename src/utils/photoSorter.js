000c63730bc7e8dd39573a3495a828570f20507e
montecamo <merrychristek@gmail.com>
2020-02-12 23:32:51 +0300

    consider margin in photos sorting

--- src/utils/photoSorter.js
+++ src/utils/photoSorter.js
@@ -11,24 +11,27 @@ const scalePhotoToFit = width => photo => ({
 
 const FIT_WIDTH = 1000;
 
-export const sortPhotos = (photos, number = 3) => {
+export const sortPhotos = (photos, options = {}) => {
+  const { columns = 1, padding = 0 } = options;
+
   const scale = scalePhotoToFit(FIT_WIDTH);
-  const columns = [];
+  const result = [];
   const heights = [];
   let currentIndex;
 
   photos.forEach((photo, i) => {
-    currentIndex = i % number;
+    currentIndex = i % columns;
 
-    if (!columns[currentIndex]) {
-      columns[currentIndex] = [];
+    if (!result[currentIndex]) {
+      result[currentIndex] = [];
       heights[currentIndex] = 0;
     }
 
     const minColumn = getMinColumn(heights);
-    columns[minColumn].push(photo);
+    result[minColumn].push(photo);
     heights[minColumn] += scale(photo).height;
+    heights[minColumn] += padding;
   });
 
-  return columns;
+  return result;
 };
